6051e787b705fe9bd3c48ff0a7c74b6a30993945
Sangeetha Kanna <sangeethakannas@gmail.com>
2021-01-04 14:32:20 +0000

    Implementing spa

--- frontendmentor/dom-manipulation.js
+++
@@ -1,9 +0,0 @@
-function emptyChildren(...args) {
-
-    for (let index in args) {
-        let node = args[index];
-        while (node.firstChild) {
-            node.removeChild(node.lastChild);
-        }
-    }
-}
